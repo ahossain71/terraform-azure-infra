277dd22de33dc133c933710736ffe55b7cb2188d
Hossain <azfar.hossain@accenturefederal.com>
2023-02-16 22:57:48 -0500

    adding az login command to the jenkins file

--- code/03-one-webserver/vars.tf
+++ code/03-one-webserver/vars.tf
@@ -6,14 +6,16 @@ variable "server_port" {
 
 variable "subscription_id"{
   description="Id of the subscription in the acount that this application would be using"
+  //value="subscription_id"
   default ="6e1001f2-25e2-42f9-b453-8075e4c87028"
 }
-/*
-variable "tenant_id"{
-  description=""
-  default=""
-}
 
+variable "tenant_id"{
+  description="tenant ID of the Azure AD in use"
+  //value="tenant_id"
+  default="40ac6e26-3eb6-4888-9080-1bac2ab44055"
+}
+/*
 variable "client_id"{
   description=""
   default=""
