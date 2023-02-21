01a2a8895c8b4a83fe0200594b65aa7e138f15cc
Hossain <azfar.hossain@accenturefederal.com>
2023-02-20 20:10:24 -0500

    added ssh protocol to the security group in the terraform

--- code/03-one-webserver/main.tf
+++ code/03-one-webserver/main.tf
@@ -91,6 +91,30 @@ resource "azurerm_network_security_group" "tftraining" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+      name                       = "HTTP"
+      priority                   = 1001
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "8080"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1010
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
   tags = {
     environment = "my-terraform-env"
   }
