9bf3b29a76930b9ee8e7058e8f3c39732ab0d827
Hossain <azfar.hossain@accenturefederal.com>
2023-02-21 16:16:28 -0500

    added output file to get key information on the generated instance-2

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -42,13 +42,13 @@ pipeline {
     stage('Configure Tomcat') {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-          withCredentials([sshUserPrivateKey(credentialsId: '1af83a22-d280-4642-a6bc-1e256e53a239', keyFileVariable: 'my-trng-devops-ssh-02')]) {  
+          withCredentials([sshUserPrivateKey(credentialsId: '1af83a22-d280-4642-a6bc-1e256e53a239', keyFileVariable: 'my-trng-devops-ssh-04')]) {  
              sh script:'''
              #!/bin/bash
               #echo "PATH is: $ANS_HOME"
               #whoami
               #echo $PATH
-              ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu -vvv --key-file ${my-trng-devops-ssh-02}
+              ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu -vvv --key-file ${my-trng-devops-ssh-04}
             '''
             }//end withCredentials
           sh "exit 0"
