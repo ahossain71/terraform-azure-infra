b0f4e4f467483b1fe143810f8b198a814577318d
Hossain <azfar.hossain@accenturefederal.com>
2023-02-20 23:08:10 -0500

    deploying modified security group rule to allow ssh access to target hosts-4

--- code/03-one-webserver/main.tf
+++ code/03-one-webserver/main.tf
@@ -80,27 +80,15 @@ resource "azurerm_network_security_group" "tftraining" {
   resource_group_name = azurerm_resource_group.tftraining.name
  
   security_rule {
-      name                       = "SSH"
-      priority                   = 1001
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "22"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-
-  security_rule {
-      name                       = "HTTP"
-      priority                   = 1020
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "8080"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
+      name                       = "allowall-sr"
+      priority                    = 100
+      direction                   = "Outbound"
+      access                      = "Allow"
+      protocol                    = "Tcp"
+      source_port_range           = "*"
+      destination_port_range      = "*"
+      source_address_prefix       = "*"
+      destination_address_prefix  = "*"
     }
 
   tags = {
