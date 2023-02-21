23a51b935b04279574223efb1b43e97424962810
Hossain <azfar.hossain@accenturefederal.com>
2023-02-21 00:24:14 -0500

    modified remote user from ubuntu to azure user in ansible palybook-2

--- code/03-one-webserver/main.tf
+++ code/03-one-webserver/main.tf
@@ -80,16 +80,16 @@ resource "azurerm_network_security_group" "tftraining" {
   resource_group_name = azurerm_resource_group.tftraining.name
  
   security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+    name                       = "HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
   tags = {
     environment = "my-terraform-env"
