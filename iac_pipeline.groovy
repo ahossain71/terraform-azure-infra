8f8646863167f3ff5133b4843cd4583de8d9dcb4
Hossain <azfar.hossain@accenturefederal.com>
2023-02-20 22:46:34 -0500

    deploying modified security group rule to allow ssh access to target hosts

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -8,14 +8,14 @@ pipeline {
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'AutoCredName', usernameVariable: 'AutoCredUser', passwordVariable: 'AutoCredPass']]) {
               sh script:'''
               #!/bin/bash
-              #chmod 755 code/03-one-webserver
-              #cd ./code/03-one-webserver
-              #echo "INITILISING TERRAFORM MODULE"
-              #terraform init
-              #echo "GENERATING TERRAFORM PLAN"              
-              #terraform plan -out=training-infra-plan
-              #echo "GENERATING TERRAFORM RESOURCES IN THE SUBSCRIPTION..."
-              #terraform apply -auto-approve
+              chmod 755 code/03-one-webserver
+              cd ./code/03-one-webserver
+              echo "INITILISING TERRAFORM MODULE"
+              terraform init
+              echo "GENERATING TERRAFORM PLAN"              
+              terraform plan -out=training-infra-plan
+              echo "GENERATING TERRAFORM RESOURCES IN THE SUBSCRIPTION..."
+              terraform apply -auto-approve
               #sleep 1m
               #echo "DESTROYING A VM RESOURCE IN THE RESOURCE GROUP"
               #terraform destroy -target=azurerm_linux_virtual_machine.tftraining -auto-approve
