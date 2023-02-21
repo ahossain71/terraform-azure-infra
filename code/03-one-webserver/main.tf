009221f047c77630880289b967445cd13a2c8d47
Hossain <azfar.hossain@accenturefederal.com>
2023-02-20 23:04:39 -0500

    deploying modified security group rule to allow ssh access to target hosts-3

--- code/03-one-webserver/main.tf
+++ code/03-one-webserver/main.tf
@@ -78,19 +78,7 @@ resource "azurerm_network_security_group" "tftraining" {
   name                = "my-terraform-nsg"
   location            = azurerm_resource_group.tftraining.location
   resource_group_name = azurerm_resource_group.tftraining.name
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8080"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
+ 
   security_rule {
       name                       = "SSH"
       priority                   = 1001
