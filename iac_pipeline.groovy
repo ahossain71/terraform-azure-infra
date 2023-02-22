0034b30d7ffeecf8388d85589424f6ee7398232e
Hossain <azfar.hossain@accenturefederal.com>
2023-02-21 23:53:06 -0500

    configured SSH credentials binding plugin to bind the ssh global credtial to a temp environment variable-16

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -44,8 +44,8 @@ pipeline {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
            withCredentials([sshUserPrivateKey(credentialsId: '1af83a22-d280-4642-a6bc-1e256e53a239', keyFileVariable: 'my-trng-devops-ssh')]) {
              //sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user azureuser --key-file ${my-trng-devops-ssh} -vvv'
-             //sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user azureuser --key-file ${my-trng-devops-ssh}'
-             sh "ansiblePlaybook credentialsId: '1af83a22-d280-4642-a6bc-1e256e53a239', disableHostKeyChecking: true, installation: 'ansible 2.11.12', inventory: '/ansible/playbooks/vars/inventory.yml', playbook: '/ansible/playbooks/tomcat-setup.yml'"  
+             sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user azureuser --key-file ${my-trng-devops-ssh} --disableHostKeyChecking true'
+             //sh "ansiblePlaybook credentialsId: '1af83a22-d280-4642-a6bc-1e256e53a239', disableHostKeyChecking: true, installation: 'ansible 2.11.12', inventory: '/ansible/playbooks/vars/inventory.yml', playbook: '/ansible/playbooks/tomcat-setup.yml'"  
             }//end withCredentials
           sh "exit 0"
          }//end catchError
