c605847f9bfd380a6efead63af27765049225161
Hossain <azfar.hossain@accenturefederal.com>
2023-03-10 12:42:52 -0500

    removed ssh key creation and association to vm resource-9

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -18,12 +18,12 @@ pipeline {
               echo "GENERATING TERRAFORM RESOURCES IN THE SUBSCRIPTION..."
               terraform apply -auto-approve
               #sleep 30s
-              terraform output -raw tls_private_key > tmp_param
+              terraform output -raw tls_private_key > id_rsa
               #echo "DESTROYING A VM RESOURCE IN THE RESOURCE GROUP"
               #terraform destroy -target=azurerm_linux_virtual_machine.tftraining -auto-approve
               '''                          
              }//end withCredentials
-             sh 'echo ${tmp_param}'
+             sh 'echo ${id_rsa}'
              sh "exit 0"
          }//end catcherror
        }   
